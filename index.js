965ff7a5600e01adcc038971f7a092e18164098d
yodaaa <01ewell0@gmail.com>
2019-04-25 12:52:28 +0900

    feat: echo /hello/:name

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { get, post, router } = require('microrouter')
+const { send } = require('micro')
+const { router, get } = require('microrouter')
 
+const hello = (req, res) => send(res, 200, `Hello ${req.params.who}`)
 
-module.exports = router(
-  get('/', () => 'hello')
+const notfound = (req, res) => send(res, 404, 'Not found route')
 
-)
+module.exports = router(get('/hello/:who', hello), get('/*', notfound))
