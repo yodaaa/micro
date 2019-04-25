667080de0238a32e15693d771de85c89fdc05935
yodaaa <01ewell0@gmail.com>
2019-04-25 13:07:53 +0900

    feat: add now(zeit)

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const { send } = require('micro')
-const { router, get } = require('microrouter')
+// const { send } = require('micro')
+// const { router, get } = require('microrouter')
+//
+// const hello = (req, res) => send(res, 200, `Hello ${req.params.who}`)
+//
+// const notfound = (req, res) => send(res, 404, 'Not found route')
+//
+// module.exports = router(get('/hello/:who', hello), get('/*', notfound))
 
-const hello = (req, res) => send(res, 200, `Hello ${req.params.who}`)
 
-const notfound = (req, res) => send(res, 404, 'Not found route')
-
-module.exports = router(get('/hello/:who', hello), get('/*', notfound))
+const app = require('express')();
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
+app.listen();
